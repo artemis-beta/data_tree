0f643b818afb5634bd7a04db4d0bdbaa3ea2f47f
artemis-beta <KXZ186@student.bham.ac.uk>
2019-08-20 17:56:38 +0100

    Patching Tests

--- tests/test_datatree.py
+++ tests/test_datatree.py
@@ -20,7 +20,7 @@ class DataTreeTest(unittest.TestCase):
         if letter_1 == letter_2:
             self.assertRaises(IndexError, dt.add_data, (None, letter_2))
 
-    @given( letters = [strategies.text( alphabet=string.ascii_uppercase, min_size=1, max_size=1) for in range(8)] )
+    @given( letters = [strategies.text( alphabet=string.ascii_uppercase, min_size=1, max_size=1) for i in range(8)] )
     def test_combine_trees(self, letters):
         dt_1, dt_2 = data_tree.DataTree(), data_tree.DataTree()
         dt_1.add_data(None, letters[0])
